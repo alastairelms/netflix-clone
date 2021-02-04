c8d1b92ae556cf6df89ff52cf4ef2f4669c0279f
alastairelms <69348143+alastairelms@users.noreply.github.com>
2021-02-04 13:49:05 +0000

    Accordion component complete

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, createContext } from 'react';
 import { Body, Title, Header, Container, Inner, Item, Frame } from './styles/accordion';
 
-export default function Accordion({ children , ...restProps }) {
+const ToggleContext = createContext();
+
+export default function Accordion({ children, ...restProps }) {
     return (
         <Container {...restProps}>
             <Inner>{children}</Inner>
@@ -9,22 +11,41 @@ export default function Accordion({ children , ...restProps }) {
     )
 }
 
-Accordion.Frame = function AccordionFrame({children, ...restProps}) {
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
     return <Frame {...restProps}>{children}</Frame>;
 }
 
-Accordion.Item = function AccordionItem({children, ...restProps}) {
-    return <Item {...restProps}>{children}</Item>;
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+    const [toggleShow, setToggleShow] = useState(false);
+
+    return (
+        <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+            <Item {...restProps}>{children}</Item>
+        </ToggleContext.Provider>
+    )
 }
 
 Accordion.Title = function AccordionTitle({ children, ...restProps }) {
     return <Title {...restProps}>{children}</Title>;
 }
 
-Accordion.Header = function AccordionHeader({ children, ...restProps}) {
-    return <Header {...restProps}>{children}</Header>;
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+    const { toggleShow, setToggleShow } = useContext(ToggleContext); 
+
+    return (
+        <Header onClick={() => setToggleShow(!toggleShow)} {...restProps}>
+            {children}
+            {toggleShow ? (
+                <img src="/images/icons/close-slim.png" alt="Close" />
+            ) : (
+                <img src="images/icons/add.png" alt="Open" />
+            )}
+        </Header>
+    )
 }
 
-Accordion.Body = function AccordionBody({ children, ...restProps}) {
-    return <Body {...restProps}>{children}</Body>;
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+    const { toggleShow } = useContext(ToggleContext);
+    
+    return toggleShow ? <Body {...restProps}>{children}</Body> : null;
 }
--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -2,7 +2,6 @@ import React from 'react'
 import jumboData from '../fixtures/jumbo.json';
 import { Jumbotron } from '../components/';
 
-
 export default function JumbotronContainer() {
     return (
         <Jumbotron.Container>
