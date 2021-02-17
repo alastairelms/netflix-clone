0e5b128ff12ef70936b09f778e0e19fe32588cfc
alastairelms <69348143+alastairelms@users.noreply.github.com>
2021-02-17 16:16:41 +0000

    Browse landing page feat. Joker

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link as ReachRouterLink } from 'react-router-dom';
-import { Background, Container, Logo, ButtonLink } from './styles/header';
+import { Group, Background, Link, Text, Container, Logo, ButtonLink, Feature, FeatureCallOut, PlayButton } from './styles/header';
 
 
 export default function Header({ bg = true, children, ...restProps }) {
@@ -11,6 +11,10 @@ Header.Frame = function HeaderFrame({ children, ...restProps }) {
     return <Container {...restProps}>{children}</Container>;
 }
 
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>;
+}
+
 Header.Logo = function HeaderLogo({ to, ...restProps }) {
     return (
         <ReachRouterLink to={to}>
@@ -19,6 +23,26 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
     )
 }
 
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+}
+
+Header.Link = function HeaderLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>;
+}
+
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
     return <ButtonLink {...restProps}>{children}</ButtonLink>;
+}
+
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>;
+}
+
+Header.FeatureCallOut = function HeaderFeeatureCallOut({ children, ...restProps }) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+}
+
+Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
+    return <PlayButton {...restProps}>{children}</PlayButton>;
 }
