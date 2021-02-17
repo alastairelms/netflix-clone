1c1c72eb34b7b1669021fb331cdc3d9a910de6ef
alastairelms <69348143+alastairelms@users.noreply.github.com>
2021-02-17 15:30:38 +0000

    Firebase authentication added

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,18 +1,34 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { Form } from '../components';
 import { HeaderContainer } from '../containers/header';
 import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
 export default function Signin() {
-    const [error, setError] = useState('');
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
+
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
     const isInvalid = password === '' || emailAddress === '';
 
     const handleSignin = (event) => {
         event.preventDefault();
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                setEmailAddress('');
+                setPassword('');
+                setError('');
+                history.push(ROUTES.BROWSE)
+            })
+            .catch((error) => setError(error.message));
     }
 
     return (
